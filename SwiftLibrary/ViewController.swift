3ccc2baab94791480cde23974366d12dade9252c
HoodsDream <christiannavarette@hoodsdreams-mbp.charter.com>
2014-12-04 19:11:09 -0800

    Initial Commit

---
+++ SwiftLibrary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftLibrary
+//
+//  Created by HoodsDream on 12/4/14.
+//  Copyright (c) 2014 HoodsDream. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
